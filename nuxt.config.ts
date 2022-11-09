6d5d8486705423064ca784614c809155506c61d6
medaka0228 <anywherefish@gmail.com>
2022-11-09 23:21:58 +0900

    ssr設定しなくて良さそう

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
     css: [
         'primevue/resources/themes/saga-blue/theme.css',
         'primevue/resources/primevue.css',
