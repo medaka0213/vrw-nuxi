aa4ea007b86cf71a2f2f3ba4632d48527168f510
medaka0228 <anywherefish@gmail.com>
2022-11-09 17:00:41 +0900

    集会の検索と詳細ページ

---
+++ components/MeetupListBlock.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+p.ml-3.my-1.text-left(v-if="items") {{items.length}}件が見つかりました
+.row
+    .p-2.text-left.col-xl-4.col-lg-6(v-for="(item, key) in items" :key="key")
+        .bg-white.p-2.py-4
+            h5.pl-3.mb-0
+                a(v-bind:href="item.itemDetailPath()")
+                    | {{item.get_jp_value('title')}}
+            .pl-3.p-2.my-1
+                MeetupListItem(:item="item")
+</template>
+
+<script>
+import MeetupListItem from "@/components/MeetupListItem"
+
+export default {
+    components: {
+        MeetupListItem
+    },
+    props: [
+        "items"
+    ]
+}
+</script>
---
+++ components/MeetupListItem.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.text-left
+    p.py-0.my-1.text-sm(v-if="item.type==='live'")
+        | 生中継の部
+    p.py-0.my-1.text-sm(v-if="item.type==='archive'")
+        | アーカイブの部
+    p.py-0.my-1.text-sm 
+        | 🗓️ 日本時間 {{ item.datetime_format()  }}
+    p.py-0.my-1.text-sm
+        | 👨‍🚀 VRChat: {{ item.user }} にJOIN
+    p.py-0.my-1.text-sm(v-if="item.get_jp_value('tweet')")
+        | 🔗 
+        a(v-bind:href="item.twitterLinkJP()" target="_blank")
+            | Twitter
+</template>
+
+<script>
+import { defineComponent, reactive, computed, onMounted } from 'vue'
+
+export default defineComponent({
+    props: ["item"]
+})
+</script>
