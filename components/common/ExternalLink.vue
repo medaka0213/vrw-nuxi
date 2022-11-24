c4a32ff70d45251e68807f9e2d2ef09a3984b5a2
medaka0228 <anywherefish@gmail.com>
2022-11-24 19:20:07 +0900

    スタイル修正

--- components/common/ExternalLink.vue
+++ components/common/ExternalLink.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 a(:href="href" target="_blank")
-    span.mr-1
-      slot
-    i.pi.pi-external-link(style="font-size: 0.5rem")
+  span.mr-1
+    slot
+  i.pi.pi-external-link(style="font-size: 0.5rem")
 AboutUsBlock
 </template>
 
