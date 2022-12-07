a5b0f4dbd4a069f52dc50995361b2999b3896679
medaka0228 <anywherefish@gmail.com>
2022-12-07 09:21:12 +0900

    footerのレイアウト修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,22 +31,22 @@ header
                 .text-center.p-3.mb-3.bg-white(v-if="$ua.browser() !== 'Safari'")
                     GoogleCalendar
 .bg-white.text-center.p-3.footer
-    .d-flex.flex-wrap.justify-content-around
-            .text-center
+    .d-flex.flex-wrap.justify-content-center.mx-auto
+            .text-center.px-3
                 span.mr-3
                     a(href="/").text-sm
                         | ホーム
                 span.mr-3
                     a(href="/about").text-sm
                         | このサイトについて
-            .text-center
+            .text-center.px-3
                 span.mr-3
                     a(href="/mission").text-sm
                         | ミッション検索
                 span.mr-3
                     a(href="/meetup").text-sm
                         | 集会検索
-            .text-center
+            .text-center.px-3
                 span.mr-3
                     a(target="_blank" href="https://t.co/nL12021X0s").text-sm
                         | Twitter
@@ -59,7 +59,7 @@ header
                     a(target="_blank" href="https://t.co/nL12021X0s").text-sm
                         | Discord
                         i.pi.pi-external-link.ml-1(style="font-size: 0.5rem")
-            .text-center
+            .text-center.px-3
                 span.mr-3
                     a(target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScESvA557taE0zYj8gzlx2cQwrUDWs3cPxDnCWlacfUWTVLwg/viewform").text-sm
                         | 参加者アンケート
@@ -117,6 +117,9 @@ header {
 .v-enter,
 .v-leave-to {
     opacity: 0;
+}
+.footer-content {
+    max-width: 1500px;
 }
 .footer a {
     color: #8a8a8a;
